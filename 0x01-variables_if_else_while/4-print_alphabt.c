25f42e5843a8b164ed4e0f6a72e80ab405d960aa
P1toah <mungaiptah35@gmail.com>
2023-06-20 04:40:26 -0700

    git commit -m

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+
 /**
-*main- execept main
-*
-*Return: 0 (Success)
-*/
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
 int main(void)
 {
-char alp[24] = abcdfghijklmnoprstuvwxyz;
-int i;
-for (i < 0 ;
-i < 24;
-i++);
-{
-putchar(alp);
-} 
-putchar("\n");
-return(0);
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
+
+
