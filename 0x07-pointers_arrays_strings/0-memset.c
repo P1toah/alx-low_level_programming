ca4f2b1f6d7d694f550856be8ae3dfa89f25961e
P1toah <mungaiptah35@gmail.com>
2023-07-03 04:48:11 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memset - fills memory with a constant byte
+ *@s: points the memory bytes
+ *@b: the constant byte
+ *@n: the bytes to be filled
+ *
+ *Description: This function fills the first n bytes of the memory area
+ *
+ *Return: memeory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+pointers to pointers and how to use them
+multidimensional arrays and how to use them
+most common C standard library functions to manipulate strings
