d2f8bb2b063d42cf402a7e95f1aef9751208ca29
P1toah <mungaiptah35@gmail.com>
2023-07-04 05:17:48 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,19 +5,18 @@
  *@a: the argument passed
  *
  *Description: This function prints a chessboard.
- *
+ *Return: 0
  */
 void print_chessboard(char (*a)[8])
 {
-int i = 0;
-int j = 0;
+int i;
+int j;
 
 for (i = 0; i < 8; i++)
 {
 for (j = 0; i < 8; j++)
 {
-char c = a[i][j];
-_putchar(c);
+_putchar(a[i][j]);
 _putchar(' ');
 }
 _putchar('\n');
