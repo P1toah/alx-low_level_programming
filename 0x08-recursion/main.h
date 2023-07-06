3001855aad3339f81c445568b7ac64226583cafb
P1toah <mungaiptah35@gmail.com>
2023-07-06 07:43:31 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int find_sqrt(int n, int start, int end);
+int check_prime(int n, int divisor);
 
 #endif
