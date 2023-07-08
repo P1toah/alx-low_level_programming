83027be2ed62ebdd8533ce87a2a65e7f503b0f8d
P1toah <mungaiptah35@gmail.com>
2023-07-07 23:39:26 -0700

    3-mul.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
+int multiply(int num1,int num2);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]);
 
