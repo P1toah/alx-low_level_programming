8f0d8edb65555bdc81533ea2ec8617a5c6b32b12
P1toah <mungaiptah35@gmail.com>
2023-07-10 00:18:49 -0700

    4-add.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int multiply(int num1,int num2);
+int multiply(int num1, int num2);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]);
+int _atoi(char *s);
+int is_positive_number(char *s);
 
 #endif
