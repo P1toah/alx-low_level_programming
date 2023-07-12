bb8a2abb31873126492ad8ec16586238e82fc94e
P1toah <mungaiptah35@gmail.com>
2023-07-12 03:34:18 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,18 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
-
-/**
- *_putchar - the putchar function
- *
- *@c: the character to be printed
- *
- *Return: 1
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 
 /**
  *create_array - creates an array
@@ -34,36 +21,4 @@ return (NULL);
 for (i = 0; i < size; i++)
 array[i] = c;
 return (array);
-}
-
-/**
- *main - the main function
- *
- *Return: void
- */
-int main(void)
-{
-unsigned int i = 0;
-unsigned int size = 10;
-char c = 'X';
-char *array;
-array = create_array(size, c);
-if (array != NULL)
-{
-for (i = 0; i < size; i++)
-_putchar(array[i]);
-_putchar('\n');
-free(array);
-}
-else
-{
-_putchar('F');
-_putchar('A');
-_putchar('I');
-_putchar('L');
-_putchar('E');
-_putchar('D');
-_putchar('\n');
-}
-return (0);
 }
