bc003a9d3a66deeedfbc4e9010c1ad9b78a228c5
p1toah <mungaiptah35@gmail.com>
2023-07-14 07:15:58 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-main.c
+++ 0x0C-more_malloc_free/3-main.c
@@ -4,43 +4,41 @@
 #include <string.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ *simple_print_buffer - prints buffer in hexa
+ *@buffer: the address of memory to print
+ *@size: the size of the memory to print
  *
- * Return: Nothing.
+ *Return: Nothing.
  */
 void simple_print_buffer(int *buffer, unsigned int size)
 {
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+unsigned int i;
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 /**
- * main - check the code
+ *main - check the code
  *
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(void)
 {
-    int *a;
-
-    a = array_range(0, 10);
-    simple_print_buffer(a, 11);
-    free(a);
-    return (0);
+int *a;
+a = array_range(0, 10);
+simple_print_buffer(a, 11);
+free(a);
+return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 unsigned int str_len(char *str);
-void simple_print_buffer(char *buffer, unsigned int size);
+void simple_print_buffer(int *buffer, unsigned int size);
 
 #endif
