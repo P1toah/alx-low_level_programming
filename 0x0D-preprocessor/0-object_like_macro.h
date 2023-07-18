f8d3dbb5ce8f531013d8f28edc561e4be18a5b82
p1toah <mungaiptah35@gmail.com>
2023-07-18 00:01:30 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
+
+#ifndef SIZE_MACRO_H
+#define SIZE_MACRO_H
+
+/* Avoid translation unit */
+
+#endif /* SIZE_MACRO_H */
